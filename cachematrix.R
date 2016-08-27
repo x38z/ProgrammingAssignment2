7ab93fcc68e71b3e42b2c13b251c4440148e14ea
x38z <x38z@nordstrom.com>
2016-08-26 21:00:57 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## create a function which accepts a null matrix argument
+makeCacheMatrix  <- function(x = matrix()) {
+## initialize the value of the matrix inverse to NULL locally.
+  s <- NULL
+  ##sets the value of inverse matrix to null globally.
+  ##sets the value of x globally.
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  ## returns x
+  get <- function() x
+  ## sets inverse matrix to global cache.
+  setinv <- function(solved) s <<- solved
+  ## gets inverse matrix from global cache.
+  getinv <- function() s
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  s <- x$getinv()
+   #if the inverse matrix exists, it gets it.
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinv(s)
+  s
 }
